e9154089f0cb35adccae2b3dbb30c81fd50f32bb
DEVMANISHOFFl <devmanishofficial@gmail.com>
2025-01-21 13:29:25 +0530

    second

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,20 +1,42 @@
 "use client";
 
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from './ui/button';
+import Image from 'next/image';
 
 const HeroSection = () => {
+
+    const imageRef = iseRef();
+
+    useEffect(() => {
+        const imageElement = imageRef.current;
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+
+            if(scrollPosition>scrollThreshold) {
+                imageElement.classList.add("scrolled");
+            }else{
+                imageElement.classList.remove("scrolled");
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+        return () =>  w
+    }, [])
+
     return (
         <div className='pb-20 px-4'>
-            <div>
-                <h1>
+            <div className='container mx-auto text-center'>
+                <h1 className='text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title'>
                     Master Your Finance <br /> with Intelligence
                 </h1>
-                <p>
+                <p className='text-xl text-gray-600 mb-8  mt-3 max-w-2xl mx-auto '>
                     An AI-powered financial management platform that help you to track, analyze, and optimise your spendings with real-time insights.
                 </p>
-                <div>
+                <div className='flex justify-center space-x-4'>
                     <Link href="/dashboard">
                         <Button size="lg" className="px-8">
                             Get Started
@@ -22,9 +44,20 @@ const HeroSection = () => {
                     </Link>
                     <Link href="/">
                         <Button size="lg" variant="outline" className="px-8">
-                            Watch Demo 
+                            Watch Demo
                         </Button>
                     </Link>
+                </div>
+                <div className='hero-image-wrapper'>
+                    <div ref={imageRef} className='hero-image'>
+                        <Image
+                            src="/banner.jpeg"
+                            width={1280}
+                            height={720}
+                            alt='Dashboard Preview'
+                            priority
+                            className='rounded-lg shadow-2xl border mx-auto ' />
+                    </div>
                 </div>
             </div>
         </div>
